06eae3832cd123729b23288716f3834ae33bf3ba
DmiitriyJarosh <dimajarosh@gmail.com>
2020-04-15 00:06:25 +0300

    Added fully functional test for DFS

--- Test/DFS/dfs_test.c
+++ Test/DFS/dfs_test.c
@@ -34,23 +34,30 @@
 
 // Contributed by Dmitrii Iarosh, SPSU
 
-// Usage:
-//
-// dfs_test matrixmarketfile.mtx
-// dfs_test < matrixfile.mtx
-
 #include "LAGraph.h"
 #include <time.h>
-// #include <sys/time.h>
 
 #define LAGRAPH_FREE_ALL
 
+#define OK(method)                                                          \
+{                                                                           \
+    GrB_Info this_info = method ;                                           \
+    if (! (this_info == GrB_SUCCESS || this_info == GrB_NO_VALUE))          \
+    {                                                                       \
+        printf ("Test failed: [%d] %s\n", this_info, GrB_error ( )) ;     \
+        LAGRAPH_FREE_ALL ;                                                  \
+        return (this_info) ;                                                \
+    }                                                                       \
+}
 
-double to_microsec(struct timespec t1, struct timespec t2)
-{
+#define GOLDEN_PATH "../test.golden.txt"
+#define TEST_PATH "../test.mtx"
+
+
+double to_microsec(struct timespec t1, struct timespec t2) {
     return
-        (t2.tv_sec  - t1.tv_sec ) * 1e6 +
-        (t2.tv_nsec - t1.tv_nsec) * 1e-3;
+            (t2.tv_sec - t1.tv_sec) * 1e6 +
+            (t2.tv_nsec - t1.tv_nsec) * 1e-3;
 }
 
 int find_root(GrB_Matrix matrix, GrB_Index size) {
@@ -68,7 +75,7 @@ int find_root(GrB_Matrix matrix, GrB_Index size) {
         GrB_Index i;
         for (i = 0; i < size; i++) {
             int q = 0;
-            GrB_Matrix_extractElement(&q, matrix, i, j);
+            OK(GrB_Matrix_extractElement(&q, matrix, i, j));
             if (q != 0) {
                 break;
             }
@@ -81,59 +88,117 @@ int find_root(GrB_Matrix matrix, GrB_Index size) {
     return 0;
 }
 
+int str_to_int(char *str, int *ptr) {
+    int num = 0;
+    int i;
+    for (i = *ptr; i < strlen(str); i++) {
+        if (str[i] > '9' || str[i] < '0') {
+            break;
+        }
+        num = num * 10 + str[i] - '0';
+    }
+    *ptr = ++i;
+    return num;
+}
 
-int main (int argc, char **argv)
-{
-    GrB_Info info ;
+
+GrB_Info dfs_test(GrB_Matrix matrix, GrB_Index size) {
+    GrB_Vector result = NULL;
+    int root = find_root(matrix, size);
+    GrB_Matrix matrix1 = NULL;
+    LAGr_Matrix_dup(&matrix1, matrix);
+//    printf("Root: %d\n", root);
+
+    struct timespec tstart = {0, 0}, tend = {0, 0};
+    clock_gettime(CLOCK_MONOTONIC, &tstart);
+    OK(LAGraph_dfs_traverse_bin_tree(&result, matrix, root, false));
+    clock_gettime(CLOCK_MONOTONIC, &tend);
+    double time = to_microsec(tstart, tend);
+    printf("Pre-order time: %f microsec\n", time);
+
+    char str[256];
+    FILE *mf = fopen(GOLDEN_PATH, "r");
+
+    if (mf == NULL) {
+        printf("File not found\n");
+        return GrB_INVALID_OBJECT;
+    }
+
+    int res_val = -1;
+    fgets(str, sizeof(str), mf);
+    int ptr = 0;
+    for (int i = 0; i < size; i++) {
+        int check_val = str_to_int(str, &ptr);
+        res_val = -1;
+        OK(GrB_Vector_extractElement(&res_val, result, i));
+        if (res_val != check_val) {
+            printf("Not equal %d != %d at position %d\n", res_val, check_val, i);
+            return GrB_INVALID_VALUE;
+        }
+    }
+
+    GrB_free(&result);
+
+    clock_gettime(CLOCK_MONOTONIC, &tstart);
+    OK(LAGraph_dfs_traverse_bin_tree(&result, matrix1, root, true));
+    clock_gettime(CLOCK_MONOTONIC, &tend);
+    time = to_microsec(tstart, tend);
+    printf("Post-order time: %f microsec\n", time);
+
+    fgets(str, sizeof(str), mf);
+    ptr = 0;
+    for (int i = 0; i < size; i++) {
+        int check_val = str_to_int(str, &ptr);
+        res_val = -1;
+        OK(GrB_Vector_extractElement(&res_val, result, i));
+        if (res_val != check_val) {
+            printf("Not equal %d != %d at position %d\n", res_val, check_val, i);
+            return GrB_INVALID_VALUE;
+        }
+    }
+
+//    int k;
+//    printf("Result: ");
+//    for (GrB_Index i = 0; i < size; i++) {
+//        GrB_Vector_extractElement(&k, result, i);
+//        printf("%d ", k);
+//    }
+//    printf("\n");
+    GrB_free(&result);
+    GrB_free(&matrix1);
+    fclose(mf);
+    return GrB_SUCCESS;
+}
+
+
+int main(int argc, char **argv) {
+    GrB_Info info;
 
     GrB_Matrix graph_matrix = NULL;
-    GrB_Vector result = NULL;
-    LAGRAPH_OK (LAGraph_init ( ));
+    LAGRAPH_OK (LAGraph_init());
 
-    if (argc > 1) {
-        char *filename = argv[1];
-        printf("matrix will be read from: %s\n", filename);
+    char *filename = TEST_PATH;
+    printf("matrix will be read from: %s\n", filename);
 
-        FILE *f = fopen(filename, "r");
-        if (f == NULL) {
-            printf("Matrix file not found: [%s]\n", filename);
-            exit(1);
-        }
-        LAGRAPH_OK(LAGraph_mmread(&graph_matrix, f));
-        fclose(f);
+    FILE *f = fopen(filename, "r");
+    if (f == NULL) {
+        printf("Matrix file not found: [%s]\n", filename);
+        exit(1);
     }
-    else
-    {
-        // Usage:  ./dfs_test < matrixfile.mtx
-        printf("matrix: from stdin\n");
+    LAGRAPH_OK(LAGraph_mmread(&graph_matrix, f));
+    fclose(f);
 
-        // read in the file in Matrix Market format from stdin
-        LAGRAPH_OK(LAGraph_mmread(&graph_matrix, stdin));
-    }
     GrB_Index n;
     LAGr_Matrix_nrows(&n, graph_matrix);
 
-    int root = find_root(graph_matrix, n);
-    printf("Root: %d\n", root);
-
-    struct timespec tstart={0,0}, tend={0,0};
-    clock_gettime(CLOCK_MONOTONIC, &tstart);
-    LAGraph_dfs_traverse_bin_tree(&result, graph_matrix, root, false);
-    clock_gettime(CLOCK_MONOTONIC, &tend);
-    double time = to_microsec(tstart, tend);
-    printf("Time: %f microsec\n", time);
-
-    int k;
-    printf("Result: ");
-    for (GrB_Index i = 0; i < n; i++) {
-        GrB_Vector_extractElement(&k, result, i);
-        printf("%d ", k);
+    if (dfs_test(graph_matrix, n) == GrB_SUCCESS) {
+        printf("Test passed!\n");
+    } else {
+        printf("Test failed!\n");
     }
-    printf("\n");
 
     GrB_free(&graph_matrix);
-    GrB_free(&result);
     LAGRAPH_FREE_ALL;
-    LAGRAPH_OK(GrB_finalize( ));
+    LAGRAPH_OK(GrB_finalize());
 }
 
